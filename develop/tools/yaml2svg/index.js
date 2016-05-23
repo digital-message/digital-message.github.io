52868598c17947c89624795d39057344b61f8c63
Sam Liu <ontouchstart@gmail.com>
2016-05-23 16:03:57 -0400

    spellcheck=false

--- develop/tools/yaml2svg/index.js
+++ develop/tools/yaml2svg/index.js
@@ -5,7 +5,11 @@ window.onload = function () {
   content = d3.select('#content').style({"margin":"1em","padding":"1em"});
 
   yaml_div = content.append('div');
-  yaml_code = yaml_div.append('textarea').attr({"rows":50}).style({"width":"100%"}).node();
+  yaml_code = yaml_div.append('textarea')
+    .attr('rows',50)
+    .attr('spellcheck', false)
+    .style('width', '100%')
+    .node();
   d3.text('sample.yaml', function (e, d) { yaml_code.value = d });
 
   button = content.append('button').html('View SVG and code').style({margin: '1em'});
